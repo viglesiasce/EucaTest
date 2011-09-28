0bd9daae107804d00f74ee8c462aa6a374d3a77c
Victor Iglesias <vic.iglesias@eucalyptus.com>
2011-09-28 01:10:29 -0700

    Made fail accept but not use its parameter

--- lib/EucaTest.pm
+++ lib/EucaTest.pm
@@ -112,11 +112,11 @@ sub new{
 
 sub fail {
   my $message = shift;
-  if (ref($message) eq "EucaTest") {
-	$message = shift;
-	}
-  push(@running_log, "^^^^^^[TEST_REPORT] FAILED - $message^^^^^^\n");
-  print("^^^^^^[TEST_REPORT] FAILED - $message^^^^^^\n");
+  #if (ref($message) eq "EucaTest") {
+	#$message = shift;
+	#}
+  push(@running_log, "^^^^^^[TEST_REPORT] FAILED ^^^^^^\n");
+  print("^^^^^^[TEST_REPORT] FAILED ^^^^^^\n");
   $fail_count++;
   if ($exit_on_fail){
   	exit(1);
